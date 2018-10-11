87a23055ae12517b11008d9e1b7e03c4118a9934
wangui.njoka <wangui.njoka@gmail.com>
2018-10-11 18:40:26 +0300

    connected postgres database

--- techolympia/settings.py
+++ techolympia/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'techolympia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'techolympia',
+        'USER':'wanguinjoka',
+        'PASSWORD':'hakiiizzy',
     }
 }
 
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -127,3 +129,5 @@ STATICFILES_DIRS = [
     ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+LOGIN_REDIRECT_URL  = 'welcome'
