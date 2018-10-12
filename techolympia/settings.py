82e5bb1a217e878f0ce6589aa6137ebac3ec97b5
wangui.njoka <wangui.njoka@gmail.com>
2018-10-12 12:12:36 +0300

    added signals- automaticatly updates profile when user is created

--- techolympia/settings.py
+++ techolympia/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -127,6 +128,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
