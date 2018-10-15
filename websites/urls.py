e4e0c9ce0fd8e91b36c124412dd14340b7f18b7f
wangui.njoka <wangui.njoka@gmail.com>
2018-10-15 16:26:55 +0300

    able to upadted and delete websites

--- websites/urls.py
+++ websites/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import SiteListView, SiteDetailView, SiteCreateView
+from .views import SiteListView, SiteDetailView, SiteCreateView, SiteUpdateView,SiteDeleteView
 from .import views
 
 urlpatterns=[
     url(r'^$', SiteListView.as_view(), name='welcome'),
     url(r'^site/(?P<pk>[0-9]+)/$', SiteDetailView.as_view(), name='site-detail'),
     url(r'^site/new/', SiteCreateView.as_view(), name='site-create'),
+    url(r'^site/(?P<pk>[0-9]+)/update/$', SiteUpdateView.as_view(), name='site-update'),
+    url(r'^site/(?P<pk>[0-9]+)/delete/$', SiteDeleteView.as_view(), name='site-delete'),
     ]
--- websites/views.py
+++ websites/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView,CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView,CreateView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Site
 # Create your views here.
 def welcome(request):
@@ -24,3 +24,27 @@ class SiteCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.developer = self.request.user
         return super().form_valid(form)
+
+class SiteUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Site
+    fields = ['title','image','description','site_url']
+
+    def form_valid(self, form):
+        form.instance.developer = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        site = self.get_object()
+        if self.request.user == site.developer:
+            return True
+        return False
+
+class SiteDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Site
+    success_url = '/'
+
+    def test_func(self):
+        site = self.get_object()
+        if self.request.user == site.developer:
+            return True
+        return False
